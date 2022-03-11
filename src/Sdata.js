764fda2e7764fbbc319bc277bb2f93ce66907df2
Avinash Kumar <ak2449746@gmail.com>
2022-03-11 16:14:52 +0530

    1st commit

---
+++ src/Sdata.js
@@ -0,0 +1,32 @@
+import web from "../src/Images/Card1.png";
+import web1 from "../src/Images/Card2.jpg";
+
+
+const Sdata = [
+    {
+        imgsrc: web,
+        title: "web development"
+    },
+    {
+        imgsrc: web,
+        title: "web development"
+    },
+    {
+        imgsrc: web,
+        title: "web development"
+    },
+    {
+        imgsrc: web1,
+        title: "web development"
+    },
+    {
+        imgsrc: web1,
+        title: "web development"
+    },
+    {
+        imgsrc: web1,
+        title: "web development"
+    }
+]
+
+export default Sdata;
